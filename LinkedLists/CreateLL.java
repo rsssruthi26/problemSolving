78238febfca3ed80e69cb5253db565a2bbac3a4c
Sai Satya Sruthi <rsssruthi26@gmail.com>
2024-06-23 20:54:34 -0400

    Create a LinkedList

---
+++ LinkedLists/CreateLL.java
@@ -0,0 +1,16 @@
+
+import java.lang.*;
+
+class CreateLL {
+
+    public static void main(String args[])
+    {
+        Node head=new Node(10);
+        Node temp1=new Node(20);
+        Node temp2=new Node(30);
+        head.next=temp1;
+        temp1.next=temp2;
+        System.out.print(head.data+"-->"+temp1.data+"-->"+temp2.data);
+
+    }
+}
